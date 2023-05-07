d309f1c4e091f01e5d849677879695135e196439
Arquitectura_Procesadores <kevinalexvivas@gmail.com>
2023-05-06 21:47:33 -0500

    Merge branch 'master' of https://github.com/Kevin20100/Graficos_Python

---
+++ 32-PSK/32_Diagram_Constellation/Constellation_32PSK.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Modem:
+    def __init__(self, M, constellation, name):
+        self.M = M
+        self.constellation = constellation
+        self.name = name
+
+class PSKModem(Modem):
+    def __init__(self, M):        
+        m = np.arange(0,M) #all information symbols m={0,1,...,M-1}
+        I = 1/np.sqrt(2)*np.cos(m/M*2*np.pi)
+        Q = 1/np.sqrt(2)*np.sin(m/M*2*np.pi)
+        constellation = I + 1j*Q #reference constellation        
+        Modem.__init__(self, M, constellation, name='32-PSK') #set the modem attributes
+
+def plot_constellation(modem):
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,5))
+    
+    # Plot de la constelación con vectores
+    ax1.axhline(0, color='k', linestyle='--')
+    ax1.axvline(0, color='k', linestyle='--')
+    ax1.set_xlabel('Real')
+    ax1.set_ylabel('Imag')
+    ax1.set_title(modem.name + ' Phase_Diagram')
+
+ 
+    ax1.grid(False)
+    
+    for point in modem.constellation:
+        ax1.arrow(0,0, point.real, point.imag, head_width=0.04, head_length=0.04, fc='b', ec='b')
+    
+    # Plot de la constelación con puntos
+    ax2.scatter(modem.constellation.real, modem.constellation.imag, s=50, color='b')
+    ax2.axhline(0, color='k', linestyle='--')
+    ax2.axvline(0, color='k', linestyle='--')
+    ax2.set_xlabel('Real')
+    ax2.set_ylabel('Imag')
+    ax2.set_title(modem.name + ' Constellation')
+    ax2.grid(False)
+    
+    plt.show()
+
+def main():
+    M = 32
+    psk_modem = PSKModem(M)
+    plot_constellation(psk_modem)
+
+if __name__ == '__main__':
+    main()
